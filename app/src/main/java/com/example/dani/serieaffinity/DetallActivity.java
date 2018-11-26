1eadc93d14a48687cff64adcc7bec27be0f19f80
df <dferrerol92@elpuig.xeill.net>
2018-11-26 18:30:38 +0100

    Commit SerieAffinity

---
+++ app/src/main/java/com/example/dani/serieaffinity/DetallActivity.java
@@ -0,0 +1,23 @@
+package com.example.dani.serieaffinity;
+
+import android.arch.lifecycle.ViewModel;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class DetallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detall);
+
+        getIntent().getExtras();
+
+
+
+
+
+
+    }
+}
